f51f2c7a9081a47f971cc700df836bd13c1211d7
o0kam1 <duzhaokun2@outlook.com>
2023-02-17 15:45:01 +0800

    fix typo

--- Launcher/main.cpp
+++ Launcher/main.cpp
@@ -1,7 +1,3 @@
-#ifndef UNICODE
-#define UNICODE
-#endif
-
 #include <windows.h>
 #include <iostream>
 #include <filesystem>
@@ -15,7 +11,7 @@ std::string cpu = "x86";
 std::string dll = "32";
 #endif
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow) {
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     if (__argc < 2) {
         std::string exePath = __argv[0];
         auto message = "Win11DarkTitleBar " + cpu + " build\n"
@@ -27,7 +23,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine,
     }
 
 #ifdef _DEBUG
-    MessageBoxA(nullptr, pCmdLine, "Win11DarkTitleBar", NULL);
+    MessageBoxA(nullptr, lpCmdLine, "Win11DarkTitleBar", NULL);
 #endif
 
     auto dllPath = (std::filesystem::current_path() / ("Hook" + dll + ".dll")).string();
@@ -38,10 +34,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine,
     ZeroMemory(&pi, sizeof(PROCESS_INFORMATION));
     si.cb = sizeof(STARTUPINFOA);
 
-    auto r = DetourCreateProcessWithDllExA(nullptr, pCmdLine, nullptr, nullptr, false, CREATE_DEFAULT_ERROR_MODE, nullptr, nullptr, &si, &pi, dllPath.c_str(), nullptr);
+    auto r = DetourCreateProcessWithDllExA(nullptr, lpCmdLine, nullptr, nullptr, false, CREATE_DEFAULT_ERROR_MODE, nullptr, nullptr, &si, &pi, dllPath.c_str(), nullptr);
     
     if (!r) {
-        auto message = "launch:\n" + (std::string)pCmdLine + "\nfailed with code " + std::to_string(GetLastError());
+        auto message = "launch:\n" + (std::string)lpCmdLine + "\nfailed with code " + std::to_string(GetLastError());
         MessageBoxA(nullptr, message.c_str(), "Win11DarkTitleBar", MB_OK | MB_ICONERROR);
     }
 
